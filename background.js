80b4ca9caeb5ab911bf25608c56bd56b2f76fbb7
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-08-17 10:06:29 +0530

    open GPT when you install the extension

--- background.js
+++ background.js
@@ -1,4 +1,28 @@
+"use strict";
 let globalTaburl;
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === "install") {
+    chrome.tabs.create({ url: "https://chat.openai.com/chat" });
+  }
+});
+
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.query({ url: "https://chat.openai.com/chat" }, function ([tab]) {
+    if (tab) {
+      chrome.tabs.update(tab.id, { active: true });
+    } else {
+      chrome.tabs.create({ url: "https://chat.openai.com/chat" });
+    }
+  });
+});
+
+// Pre-fill prompt input with user input using omnibox
+chrome.omnibox.onInputEntered.addListener((text) => {
+  chrome.tabs.create({
+    url: `https://chat.openai.com/?AIPRM_Prompt=${encodeURIComponent(text)}`,
+  });
+});
 
 // Listen for tab updates and inject content.js file
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
